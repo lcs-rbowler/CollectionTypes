f13403ead897ff2d3ec217599129329b7403e86b
Ryan Bowler <rbowler@lcs.on.ca>
2018-02-13 22:24:24 -0500

    Exercise partially done but getting unknown error

--- Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
@@ -9,16 +9,18 @@
 
 import Foundation
 
-
+var grades : [Double] = []
 /*:
  2. Now populate the array with eight random grades between 60 and 100.
  */
-
+for _ in 1...8 {
+    grades.append( Double( arc4random_uniform(40) + 60))
+}
 
 /*:
  3. Print a list of each array index and the associated value.
  */
-
+print(grades)
 
 /*:
  4. Determine and print the highest grade.
